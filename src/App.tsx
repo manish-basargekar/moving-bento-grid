f7fa689d2e65c04fa2720f4b008671450f0ded87
Vinayyy <vinaybasargekar@gmail.com>
2025-03-01 19:17:50 +0530

    chore: Refactor tab slider position calculation in App component (#3)
    * chore: Refactor tab slider position calculation in App component
    
    * fix: Improved responsiveness and used userefs
    
    * fix: bg changed
    
    * refactor: Simplify tab slider position calculation and move logic to Navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -3,34 +3,31 @@ import Layout from "./components/layout";
 import Navbar from "./components/navbar";
 
 export enum TabKey {
-  Home = "Home",
-  Work = "Work",
-  Blog = "Blog",
-  Contact = "Contact",
+	Home = "Home",
+	Work = "Work",
+	Blog = "Blog",
+	Contact = "Contact",
 }
 
 function App() {
-  const [tab, setTab] = useState<TabKey>(TabKey.Home);
+	const [tab, setTab] = useState<TabKey>(TabKey.Home);
+	const [x, setX] = useState(0);
+	const [w, setW] = useState(0);
 
-  const tabOffsets: { [key in TabKey]: number } = {
-    Home: 0,
-    Work: 1,
-    Blog: 2,
-    Contact: 3,
-  };
 
-  const baseX = 520;
-  const baseW = 221.5;
-
-  const x = baseX + tabOffsets[tab] * baseW;
-  const w = baseW;
-
-  return (
-    <main className="bg-[#f7f2f2]">
-      <Navbar tab={tab} setTab={setTab} left={x} sliderWidth={w} />
-      <Layout tab={tab} setTab={setTab} left={x} sliderWidth={w} />
-    </main>
-  );
+	return (
+		<main className="bg-[#f7f2f2] ">
+			<Navbar
+				tab={tab}
+				setTab={setTab}
+				left={x}
+				sliderWidth={w}
+				setX={setX}
+				setW={setW}
+			/>
+			<Layout tab={tab} setTab={setTab} left={x} sliderWidth={w} />
+		</main>
+	);
 }
 
 export default App;
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,52 +1,81 @@
 import { TabKey } from "../App";
+import {  useEffect, useRef } from "react";
 
 interface NavbarProps {
-  tab: TabKey;
-  setTab: (tab: TabKey) => void;
-  left?: number;
-  sliderWidth?: number;
+	tab: TabKey;
+	setTab: (tab: TabKey) => void;
+	left?: number;
+	sliderWidth?: number;
+	setX: (x: number) => void;
+	setW: (w: number) => void;
 }
 
-const Navbar: React.FC<NavbarProps> = ({
-  tab,
-  setTab,
-  left = 0,
-  sliderWidth = 0,
-}) => {
-  const tabs = [
-    { key: TabKey.Home, label: "Home" },
-    { key: TabKey.Work, label: "Projects" },
-    { key: TabKey.Blog, label: "Writing" },
-    { key: TabKey.Contact, label: "Contact" },
-  ];
+const Navbar = ({ tab, setTab, left = 0, sliderWidth = 0 , setX, setW}:NavbarProps) => {
+		const tabs = [
+			{ key: TabKey.Home, label: "Home" },
+			{ key: TabKey.Work, label: "Projects" },
+			{ key: TabKey.Blog, label: "Writing" },
+			{ key: TabKey.Contact, label: "Contact" },
+		];
 
-  return (
-    <div className="bg-black-1 text-white mx-auto sticky top-0 z-[10000] backdrop-saturate-180 backdrop-blur-lg md:display-none pt-2">
-      <div className="bg-white max-w-[900px] m-auto rounded-full  text-1.8rem border-b border-gray-200 p-[3px]">
-        <div className="flex rounded-33px p-2 justify-between items-center text-[#111827] max-w-[900px] mx-auto">
-          {tabs.map(({ key, label }) => (
-            <div
-              key={key}
-              className={`flex items-center h-8 flex-1 cursor-pointer justify-center ${
-                tab === key ? "text-black" : "text-black-300"
-              }`}
-              onClick={() => setTab(key)}
-            >
-              {label}
-            </div>
-          ))}
-          <div
-            className="absolute h-10 bg-[#fc205e]/40 border border-[#fc205e] rounded-full z-20"
-            style={{
-              left: `${left}px`,
-              width: `${sliderWidth}px`,
-              transition: "left 0.38s cubic-bezier(0.5, 0, 0, 0.75)",
-            }}
-          ></div>
-        </div>
-      </div>
-    </div>
-  );
-};
+			const tabRefs = useRef<Record<TabKey, HTMLDivElement | null>>({
+				[TabKey.Home]: null,
+				[TabKey.Work]: null,
+				[TabKey.Blog]: null,
+				[TabKey.Contact]: null,
+			});
+
+			useEffect(() => {
+				const calculateSliderPosition = () => {
+					const currentTabRef = tabRefs.current[tab];
+
+					if (currentTabRef) {
+						const rect = currentTabRef.getBoundingClientRect();
+						setX(rect.left);
+						setW(rect.width);
+					}
+				};
+
+				calculateSliderPosition();
+
+				window.addEventListener("resize", calculateSliderPosition);
+
+				return () => {
+					window.removeEventListener("resize", calculateSliderPosition);
+				};
+			}, [tab]);
+		return (
+			<div
+				className="navbar bg-black-1 text-white mx-auto sticky top-0 z-[10000] backdrop-saturate-180 backdrop-blur-lg md:display-none pt-2"
+			>
+				<div className="bg-white max-w-[900px] m-auto rounded-full text-1.8rem border-b border-gray-200 p-[3px]">
+					<div className="flex rounded-33px p-2 justify-between items-center text-[#111827] max-w-[900px] mx-auto">
+						{tabs.map(({ key, label }) => (
+							<div
+								key={key}
+								ref={(el) => {
+									tabRefs.current[key] = el;
+								}}
+								className={`tab flex items-center h-8 flex-1 cursor-pointer justify-center ${
+									tab === key ? "text-black" : "text-black-300"
+								}`}
+								onClick={() => setTab(key)}
+							>
+								{label}
+							</div>
+						))}
+						<div
+							className="absolute h-10 bg-[#fc205e]/40 border border-[#fc205e] rounded-full z-20"
+							style={{
+								left: `${left}px`,
+								width: `${sliderWidth}px`,
+								transition: "left 0.38s cubic-bezier(0.5, 0, 0, 0.75)",
+							}}
+						></div>
+					</div>
+				</div>
+			</div>
+		);
+	};
 
 export default Navbar;
